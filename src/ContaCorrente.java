c6971894d1fdc6045385833ea9a382dc1f003af0
rodrigofreire <rodrigosharp99@gmail.com>
2025-12-17 11:20:04 -0300

    Estudos de sintaxe

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+import java.time.LocalDate;
+import java.util.List;
 
 public class ContaCorrente {
+
+    Integer numero;
+    Integer agencia;
+    String nomeCliente;
+    LocalDate dataNascimento;
+    private Double saldo;
+    boolean ativo;
+
+    Double consultarSaldo (){
+
+        return null;
+    }
+
+    List consultarExtrato (LocalDate dataInicio, LocalDate dataFim){
+        return null;
+    }
+
+    void cancelar (String justificava){}
+         //algoritmo irá manipular o atributo
+         //ativo para que se torne false
+
+    void transferir (ContaCorrente contaDestino, double valorTransferencia){
+
+    }
+
+    void sacar (double valorSolicitado){
+        //não solictei logica
+    }
+
 }
+
+
