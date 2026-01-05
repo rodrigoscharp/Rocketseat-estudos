2fc2d853bdf3ff291778c71a88387bec0e5a06ff
Rodrigo Scharp <rodrigosharp99@gmail.com>
2026-01-05 14:58:22 -0300

    Wrappeers

---
+++ src/wrapeers/TiposWrappeers.java
@@ -0,0 +1,14 @@
+package wrapeers;
+
+import java.net.StandardSocketOptions;
+import java.sql.SQLOutput;
+
+public class TiposWrappeers {
+    public static void main(String[] args){
+        double numeroDouble = 123.56;
+        int numeroInt = (int) numeroDouble; // conversão explicita
+        System.out.println(numeroInt);
+
+    }
+}
+
